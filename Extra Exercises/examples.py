8bbcebd4947837dc081979fb3284e7c595bf9ccd
Luiz Pavanello <luizpavanello@gmail.com>
2021-10-13 16:04:31 -0300

    Just some exercises...

---
+++ Extra Exercises/examples.py
@@ -0,0 +1,21 @@
+"""CS50"""
+
+# print('Hello from CS50')
+
+# answer = input('Whats is your name? ')
+# print(f'Hello {answer}!')
+
+# counter += 1
+
+# x= 3
+# y =2
+# if x < y:
+#     print(f'{x} is less than {y}')
+# elif x == y:
+#     print('{x} is equal to {y}')    
+# else:
+#     print(f'{x} is greater than {y}')
+    
+# for i in range(1,3):
+#     print("I'm Python")    
+
