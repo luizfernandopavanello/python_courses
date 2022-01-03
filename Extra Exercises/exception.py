51f440d59be0ce4aad7b38b2102017c978385462
Luiz Pavanello <luizpavanello@gmail.com>
2022-01-03 17:08:13 -0300

    Add exceptions's exampe

---
+++ Extra Exercises/exception.py
@@ -0,0 +1,13 @@
+'''exception => events detected during execution that interrupt the normal flow of the program'''
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    result = numerator / denominator
+    print(result)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("You must enter only number!")
+except Exception:
+    print("Numerator and denominator must be valid numbers!")
