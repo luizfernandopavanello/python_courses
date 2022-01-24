82df822be8352b0542e6ebf13c36a9c7d4598efb
Luiz Pavanello <luizpavanello@gmail.com>
2022-01-24 14:48:13 -0300

    Format strings

---
+++ Extra Exercises/string_format.py
@@ -0,0 +1,13 @@
+
+# str.format() = optional metho that gived users
+#                more control when displaying output
+#                positional argument
+
+number = int(input('The number is: '))
+
+print(f"The number is {number:.3f}")
+print(f"The number is {number:,}")
+print(f"The number is {number:b}") # binary
+print(f"The number is {number:o}") # octo
+print(f"The number is {number:X}") 
+print(f"The number is {number:E}")
