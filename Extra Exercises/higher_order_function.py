b85295c5d445f2d815b81474de2e48a1a810b660
Luiz Pavanello <luizpavanello@gmail.com>
2022-01-24 20:32:21 -0300

    Higher Order Function

---
+++ Extra Exercises/higher_order_function.py
@@ -0,0 +1,19 @@
+"""
+Higher Order Function = a function that either:
+                        1. accepts a function as an argument
+                            ||
+                        2. returns a function (also treated as objects)
+"""
+
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
+
+def hello(func):
+    text = func("Hello")
+    print(text)
+
+hello(loud)
+hello(quiet)
