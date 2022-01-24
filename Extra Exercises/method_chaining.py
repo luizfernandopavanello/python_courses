704e95540de25d23ccdfa4765a23cfcc023fd968
Luiz Pavanello <luizpavanello@gmail.com>
2022-01-24 17:58:05 -0300

    Chaining method

---
+++ Extra Exercises/method_chaining.py
@@ -0,0 +1,30 @@
+"""
+→ Method Chaining: calling multiple methods sequentially;
+                   each call performs an action on the same object and returns self
+"""
+
+class Car:
+
+    def turn_on(self):
+        print("You start the engine.")
+        return self # has to return itself
+
+    def drive(self):
+        print("You drive the car.")
+        return self
+
+    def brake(self):
+        print("You step on the brakes.")
+        return self
+
+    def turn_off(self):
+        print("You turn off the engine.")
+        return self
+
+car = Car()
+
+car.turn_on()\
+    .drive()\
+    .brake()\
+    .turn_off()
+
