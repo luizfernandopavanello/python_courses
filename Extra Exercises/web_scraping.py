c0a778417f8f5ccae37f62812fff632c4e676145
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-05-29 15:28:57 -0300

    Finish the web scraping file...

--- Extra Exercises/web_scraping.py
+++ Extra Exercises/web_scraping.py
@@ -1,8 +1,26 @@
 # first import the module
-import webbrowser
+import requests, sys, webbrowser, bs4
 
-url = 'https://docs.python.org/'
+# displey the text while downloanding the search result page
+print('Searching...')
 
-webbrowser.get('mozila').open_new(url, new=0, autoraise=True)
+res = requests.get('https://duckduckgo.com/?t=ffab&q=' 'https://pypi.org/search/?q='
++ ' '.join(sys.argv[1:]))
+
+res.raise_for_status()
 
 
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+
+# Open a browser tab for each result.
+linkElems = soup.select('.package-snippet')
+
+numOpen = min(5, len(linkElems))
+
+for i in range(numOpen):
+	urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+	print('Opening', urlToOpen)
+	webbrowser.open(urlToOpen)
+	
