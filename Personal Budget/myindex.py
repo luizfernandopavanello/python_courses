e0f5ac4809244b8e9be1c3b2297e309295147587
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2023-01-25 17:55:18 -0300

    Started new project - Automations and Personal Buget

---
+++ Personal Budget/myindex.py
@@ -0,0 +1,34 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from app import *
+from components import sidebar
+
+
+
+# =========  Layout  =========== #
+content = html.Div(id="page-content")
+
+
+app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ])
+    ])
+
+
+
+
+], fluid=True,)
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(port=8051, debug=True)
