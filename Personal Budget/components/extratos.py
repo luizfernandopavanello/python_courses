1c743c589009b0a40235655b4a8c785c481e5eef
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2023-01-26 17:18:06 -0300

    Made the Extrato's page where we gonna see the graphics with the expenses.

--- Personal Budget/components/extratos.py
+++ Personal Budget/components/extratos.py
@@ -12,9 +12,27 @@ from app import app
 
 # =========  Layout  =========== #
 layout = dbc.Col([
-        html.H5('Extratos')
+    dbc.Row([
+        html.Legend("Tbelaa de despesas"),
+        html.Div(id="tabela-despesas", className="dbc"),
+    ]),
 
-], )
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id="bar-graph", style={"margin-rigt": "20px"}), 
+        ], width=9),
+
+        dbc.Col([
+            dbc.Card(
+                dbc.CardBody([
+                    html.HH4("Despesas"),
+                    html.Legend("R$ -", id="valor_despesa_card", style={'font-size': '60px'}),
+                    html.H6("Total de despesas"),
+                ], style={'text-align': 'center', 'padding-top': '30px'})
+            )
+        ], width=3),
+    ]),
+], style={'padding': '10px'})
 
 # =========  Callbacks  =========== #
 # Tabela
