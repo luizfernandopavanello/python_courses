46b2547799f601c6a80ff8d5353c1f1fbfe54d93
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2023-01-27 09:57:50 -0300

    Handling data to send the revenues

--- Personal Budget/components/sidebar.py
+++ Personal Budget/components/sidebar.py
@@ -247,6 +247,7 @@ def toggle_modal(n1, is_open):
         return not is_open
 
 
+# Enviar Form receita
 @app.callback(
     Output('store-receitas', 'data'),
     Input('salvar_receita', 'n_clicks'),
@@ -262,5 +263,19 @@ def toggle_modal(n1, is_open):
 def salve_form_receita(n, descricao, valor, date, switches, categoria, dict_receitas):
     # import pdb
     # pdb.set_trace()
+
+    df_receitas = pd.DataFrame(dict_receitas)
     
-    return {}
+    if n and not(valor == "" or valor == None):
+        valor = round(float(valor), 2)
+        date = pd.to_datetime(date).date()
+        categoria = categoria[0] if type(categoria) == list else categoria
+        
+        recebido = 1 if 1 in switches else 0
+        fixo = 1 if 2 in switches else 0
+
+        df_receitas.loc[df_receitas.shape[0]] = [valor, recebido, fixo, date, categoria, descricao]
+        df_receitas.to_csv("df_receitas.csv")
+
+    data_return = df_receitas.to_dict()
+    return data_return
